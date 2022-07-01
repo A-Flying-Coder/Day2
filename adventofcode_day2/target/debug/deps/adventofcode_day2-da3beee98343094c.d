b540cfc65c21ca6defdebd9a0de644a49f30c08d
cockpitcoding <75435783+cockpitcoding@users.noreply.github.com>
2022-07-01 13:05:50 +0100

    test commit

---
+++ adventofcode_day2/target/debug/deps/adventofcode_day2-da3beee98343094c.d
@@ -0,0 +1,5 @@
+c:\Users\charl\projects\advent of code\Day2\adventofcode_day2\target\debug\deps\adventofcode_day2-da3beee98343094c.rmeta: src\main.rs
+
+c:\Users\charl\projects\advent of code\Day2\adventofcode_day2\target\debug\deps\adventofcode_day2-da3beee98343094c.d: src\main.rs
+
+src\main.rs:
